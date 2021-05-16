e7f40b0106df1e5befa3f9a098f86b5cff400cd6
Chad Tarpey <chadtarpey@gmail.com>
2021-05-16 17:05:13 -0500

    insatlled next/auth for login with facebook

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ import HeaderIcon from "../components/HeaderIcon"
 import {
     BellIcon,
     ChatIcon,
-    ChevronDownicon,
+    ChevronDownIcon,
     HomeIcon,
     UserGroupIcon,
     ViewGridIcon,
@@ -28,14 +28,14 @@ const Header = () => {
                     layout={"fixed"}
                 />
                 <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
-                    <SearchIcon className="h-6 text-gray-600" />
-                    <input className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink" type="text" placeholder="Search Facebook" />
+                    <SearchIcon className=" h-6 text-gray-600" />
+                    <input className="hidden md:inline-flex  ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink" type="text" placeholder="Search Facebook" />
                 </div>
             </div>
             {/* center */}
             <div className="flex justify-center flex-grow">
-                <div className="flex space-x-6 md:space-x-2">
-                    <HeaderIcon Icon={HomeIcon} />
+                <div className=" flex space-x-6 md:space-x-2">
+                    <HeaderIcon active Icon={HomeIcon} />
                     <HeaderIcon Icon={FlagIcon} />
                     <HeaderIcon Icon={PlayIcon} />
                     <HeaderIcon Icon={ShoppingCartIcon} />
@@ -44,6 +44,15 @@ const Header = () => {
             </div>
 
             {/* right */}
+            <div className="flex items-center sm:space-x-2 justify-end">
+                {/* Profile pic */}
+
+                <p className="whitespace-nowrap font-semibold pr-3">Chad Tarpey</p>
+                <ViewGridIcon className="icon" />
+                <ChatIcon className="icon" />
+                <BellIcon className="icon" />
+                <ChevronDownIcon className="icon" />
+            </div>
         </div>
     )
 }
